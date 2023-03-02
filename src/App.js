7185da94db164156c5c08bc8b36637820bc2ce92
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-03-02 23:01:08 +0100

    iteration 1/2/3/4/bonus weird

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import './App.css';
-import countries from './countries.json';
+// import countries from './countries.json';
 import NavBar from './components/NavBar';
 import CountriesList from './components/CountriesList';
 import { Route, Routes } from 'react-router-dom';
 import CountryDetails from './components/CountryDetails';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const url = 'https://ih-countries-api.herokuapp.com/countries';
+  const [data, setData] = useState([]);
+
+  const getCountries = async () => {
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, [data]);
+
   return (
     <div className="App">
       <header className="navbar navbar-dark bg-primary mb-3">
         <NavBar />
       </header>
-      <main>
-        <div className="container">
-          <div className="row">
-            <CountriesList countriesList={countries} />
-            <Routes>
-              <Route
-                path="/:idAlpha"
-                element={<CountryDetails countriesList={countries} />}
-              />
-            </Routes>
+      {data.length && (
+        <main>
+          <div className="container">
+            <div className="row">
+              <CountriesList countriesList={data} />
+              <Routes>
+                <Route
+                  path="/:idAlpha"
+                  element={<CountryDetails countriesList={data} />}
+                />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      )}
     </div>
   );
 }
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
 
 
 function CountryDetails({countriesList}) {
 
 let {idAlpha} = useParams()
+const url = "https://ih-countries-api.herokuapp.com/countries/"
 
+//using without Axios
 const country = countriesList.find((country) => country.alpha3Code ===  idAlpha)
 
 
-const ArrayBorders = country.borders ; 
+// i have a problem it doesnt work the loading is weird... try to replace "country to oneCountry below"
+  // const [oneCountry, setOneCountry] = useState([]);
 
-console.log(ArrayBorders)
+  // const getOneCountry= async () => {
+  //   try {
+  //     const response = await axios.get(url+country.alpha3Code);
+  //     console.log(response.data)
+  //     setOneCountry(response.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getOneCountry();
+  // }, []);
+
 
 
   return (
-    
-
 <div className="col-7">
-          <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={country.name.common} />
+    
+          <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={`${country.name.common}`} />
             <h1>{country.name.common}</h1>
             <table className="table">
               <tbody>
@@ -33,16 +50,18 @@ console.log(ArrayBorders)
                   <td>
                     <ul>
                       {
-                        country.borders.map((border)=>(
-                          <li key={border}><a href={`/${border}`}>
+                        country &&
+                        country.borders.map((border)=>{
+                         
+                        const borderCountries = countriesList.filter(countryMap => countryMap.alpha3Code === border)
+
+                         return  <li key={border}><a href={`/${border}`}>
                             
-                            {border}
+                         { borderCountries.map(elem => elem.name.common )}
                             
                             </a></li>
-                        ))
-                            
+                        })         
                       }
-                      
                     </ul>
                   </td>
                 </tr>
